d05ed84731cc06b340923ba8901b9c1957cca5c2
AdrianoPedro7 <adriano.p.damiao@gmail.com>
2023-02-14 20:14:42 -0300

    operadores ternarios

---
+++ Java/tipos-variaveis/src/operadoresTernarios.java
@@ -0,0 +1,16 @@
+public class operadoresTernarios {
+
+    public static void main() {
+        int a, b;
+        a = 6;
+        b = 5;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+        
+    }
+
+   
+}
